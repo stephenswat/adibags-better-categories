df8b45e427fd5e3082544b16f1a912c2b67b8133
Stephen Nicholas Swatman <stephen@v25.nl>
2021-01-22 01:46:10 +0100

    Add the source code for the addon

---
+++ src/data/Classes.lua
@@ -0,0 +1,37 @@
+local _, data = ...
+
+data.classes = {
+    [0] = {
+        name = "Consumable",
+        subclasses = {
+            [-3] = "Temporary Enchantments",
+            [ 0] = nil,
+            [ 1] = "Potions",
+            [ 2] = "Elixirs",
+            [ 3] = "Flasks",
+            [ 4] = "Scrolls",
+            [ 5] = "Food & Drinks",
+            [ 6] = "Permanent Enchantments",
+            [ 7] = "Bandages",
+        },
+    },
+    [7] = {
+        name = "Trade Goods",
+        subclasses = {
+            [ 0] = nil,
+            [ 1] = "Parts",
+            [ 2] = "Explosives",
+            [ 3] = "Devices",
+            [ 5] = "Cloth",
+            [ 6] = "Leather",
+            [ 7] = "Metal & Stone",
+            [ 8] = "Meat",
+            [ 9] = "Herbs",
+            [12] = "Enchanting",
+            [90] = "Gems",
+            [91] = "Poison Reagents",
+            [92] = "Cooking Ingredients",
+            [93] = "Elemental",
+        },
+    },
+}
---
+++ src/filters/Core.lua
@@ -0,0 +1,56 @@
+local _, data = ...
+
+local adi = LibStub('AceAddon-3.0'):GetAddon('AdiBags')
+
+local filter = adi:RegisterFilter('BetterCategories', 5, 'ABEvent-1.0')
+
+filter.uiName = 'Better Item Categories'
+filter.uiDesc = 'Sets more granular categories (from WowHead) for items.'
+
+function filter:OnInitialize()
+    self.db = adi.db:RegisterNamespace('BetterCategories', {})
+end
+
+function filter:GetOptions()
+    return {}, adi:GetOptionHandler(self, true)
+end
+
+function filter:GetClassIds(itemId)
+    local entry = data.overrides[itemId]
+
+    if entry == nil then
+        entry = data.items[itemId]
+    end
+
+    if entry == nil then
+        return nil
+    end
+
+    return entry.class, entry.subclass
+end
+
+function filter:GetClassName(classId, subclassId)
+    local class = data.classes[classId]
+
+    if class == nil then
+        return nil
+    end
+
+    local subclass = class.subclasses[subclassId]
+
+    if subclass == nil then
+        return class.name
+    end
+
+    return subclass, class.name
+end
+
+function filter:Filter(slot)
+    local classId, subclassId = self:GetClassIds(slot.itemId)
+
+    if classId == nil then
+        return nil
+    end
+
+    return self:GetClassName(classId, subclassId)
+end
